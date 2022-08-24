b39b59b9b6c04b6073c87da9b7b1105bab8caff6
Bo Latt <bolattny@gmail.com>
2022-08-24 17:31:30 -0400

    add more cells feature

--- main.js
+++ main.js
@@ -35,9 +35,18 @@ const resetBtn = document.querySelector("#reset");
 const paintAllBtn = document.querySelector("#paint-all");
 const oneRandomBtn = document.querySelector("#one-random");
 const allRandomBtn = document.querySelector("#all-random");
+const addMoreCellsBtn = document.querySelector("#add-more-cells");
 
 resetBtn.addEventListener("click", (e) => {
   curColor.style.backgroundColor = "white";
+  main.innerHTML = "";
+  main.style.gridTemplateColumns = "repeat(10,50px)";
+  main.style.gridAutoRows = "50px";
+  for (let i = 0; i < 100; i++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    main.append(div);
+  }
   paintAll("white");
 });
 
@@ -46,7 +55,8 @@ paintAllBtn.addEventListener("click", () => {
 });
 
 oneRandomBtn.addEventListener("click", () => {
-  let color = randomColor();
+  let [r, g, b] = randomColor();
+  let color = `rgba(${r}, ${g}, ${b})`;
   curColor.style.backgroundColor = color;
   paintAll(color);
 });
@@ -55,24 +65,41 @@ allRandomBtn.addEventListener("click", () => {
   paintAll(null, false);
 });
 
+let defaultColumns = 10;
+addMoreCellsBtn.addEventListener("click", () => {
+  const cells = document.querySelectorAll(".cell");
+  const currentWidthOrHeight = document.querySelector(".cell").offsetWidth;
+  defaultColumns += 10;
+  main.style.gridTemplateColumns = `repeat(autofill,${
+    currentWidthOrHeight / 2
+  }px)`;
+  main.style.gridAutoRows = `${currentWidthOrHeight / 2}px`;
+  console.log(currentWidthOrHeight);
+  for (let cell of cells) {
+    cell.style.width = currentWidthOrHeight / 2 + "px";
+    cell.style.height = currentWidthOrHeight / 2 + "px";
+  }
+  for (let i = 0; i < 100; i++) {
+    const div = document.createElement("div");
+    div.style.width = currentWidthOrHeight / 2 + "px";
+    div.style.height = currentWidthOrHeight / 2 + "px";
+    div.classList.add("cell");
+    main.append(div);
+  }
+});
+
 function randomColor() {
-  const colors = [
-    "black",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "purple",
-    "white",
-  ];
-  const random = Math.floor(Math.random() * colors.length);
-  return colors[random];
+  const first = Math.floor(Math.random() * 256);
+  const second = Math.floor(Math.random() * 256);
+  const third = Math.floor(Math.random() * 256);
+  return [first, second, third];
 }
 
 function paintAll(color, isOneRandomColor = true) {
   const cells = document.querySelectorAll(".cell");
   for (let cell of cells) {
-    cell.style.backgroundColor = isOneRandomColor ? color : randomColor();
+    let [r, g, b] = randomColor();
+    let random = `rgba(${r}, ${g}, ${b})`;
+    cell.style.backgroundColor = isOneRandomColor ? color : random;
   }
 }
